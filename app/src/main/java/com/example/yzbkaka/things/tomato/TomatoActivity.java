1db5bada25957452a950ab9ca60c8f42a3f03cd7
BriAlpha <2745031785@qq.com>
2023-12-11 17:55:32 +0800

    update tomato0.3(ring, vibarate, notice)

--- app/src/main/java/com/example/yzbkaka/things/tomato/TomatoActivity.java
+++ app/src/main/java/com/example/yzbkaka/things/tomato/TomatoActivity.java
@@ -1,18 +1,26 @@
 package com.example.yzbkaka.things.tomato;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.yzbkaka.things.R;
 import com.example.yzbkaka.things.Record.RecordActivity;
+import com.example.yzbkaka.things.Record.RecordListActivity;
 import com.example.yzbkaka.things.db.UserRecord;
 
 import java.util.Calendar;
@@ -29,6 +37,9 @@ public class TomatoActivity extends AppCompatActivity {
     private Spinner categories;
     private boolean isTimerRunning = false;
     private Calendar calendar;
+    private MediaPlayer mediaPlayer;
+    private Vibrator vibrator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +49,11 @@ public class TomatoActivity extends AppCompatActivity {
         back = (Button)findViewById(R.id.back);
         timerTextView = findViewById(R.id.timerTextView);
         startButton = findViewById(R.id.startButton);
-        
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                onBackPressed();
             }
         });
 
@@ -50,7 +61,7 @@ public class TomatoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isTimerRunning) {
-                    stopTimer();
+                    stopConfirmationDialog();
                 } else {
                     startTimer();
                 }
@@ -59,6 +70,11 @@ public class TomatoActivity extends AppCompatActivity {
     }
 
     private void startTimer() {
+        // 如果计时器已经在运行，不再创建新的计时器
+        if (isTimerRunning && countDownTimer != null) {
+            return;
+        }
+
         // 获取用户输入的时间和活动
         action = findViewById(R.id.editAction);
         editTextTime = findViewById(R.id.editTextTime);
@@ -98,10 +114,11 @@ public class TomatoActivity extends AppCompatActivity {
                     userRecord.setYear(year);
                     userRecord.setMonth(month);
                     userRecord.setDay(day);
-
                     userRecord.save();
 
-
+                    stopTimer();
+                    //FinishMessage
+                    showCompletionMessage();
                 }
             }.start();
 
@@ -132,11 +149,136 @@ public class TomatoActivity extends AppCompatActivity {
         isTimerRunning = false;
     }
 
+    private void showCompletionMessage() {
+        // 弹出 Toast 消息
+        Toast.makeText(TomatoActivity.this, "Task completed! Your record was loading!", Toast.LENGTH_LONG).show();
+
+        // 播放铃声
+        playRingtone();
+
+        // 执行震动
+        vibrate();
+
+        // 创建按钮
+        Button toRecordActivityButton = new Button(this);
+        toRecordActivityButton.setText("View all your work");
+        // 设置按钮的背景颜色为橙色
+        toRecordActivityButton.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+
+        // 设置按钮的点击监听器
+        toRecordActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 停止播放铃声
+                stopRingtone();
+                // 停止震动
+                stopVibration();
+                // 在按钮点击时执行跳转逻辑
+                Intent intent = new Intent(TomatoActivity.this, RecordListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        layoutParams.addRule(RelativeLayout.BELOW, R.id.startButton); // 改成你想要放置按钮的位置
+
+        ((RelativeLayout) findViewById(R.id.RelativeLayout)).addView(toRecordActivityButton, layoutParams);
+    }
+
+    private void playRingtone() {
+        // 初始化 MediaPlayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.ring); // 替换为你的铃声资源
+
+        // 设置循环播放
+        mediaPlayer.setLooping(false);
+
+        // 开始播放
+        mediaPlayer.start();
+    }
+    private void stopRingtone() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+
+    private void vibrate() {
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        // 震动 500 毫秒（可以根据需要调整）
+        if (vibrator != null) {
+            vibrator.vibrate(500);
+        }
+    }
+    private void stopVibration() {
+        if (vibrator != null) {
+            vibrator.cancel();
+        }
+    }
+
     private void updateTimerText(long millisUntilFinished) {
         int minutes = (int) (millisUntilFinished / 1000) / 60;
         int seconds = (int) (millisUntilFinished / 1000) % 60;
 
         String timeLeftFormatted = String.format("%02d:%02d", minutes, seconds);
         timerTextView.setText(timeLeftFormatted);
+    }
+
+    //退出提示
+    @Override
+    public void onBackPressed() {
+        if (isTimerRunning) {
+            showExitConfirmationDialog();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void showExitConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme_Dialog);
+        builder.setTitle("Confirmation")
+                .setMessage("Are you want to cancel the timer? Finish your job will record it in your list.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 用户确认退出，停止计时器并关闭应用
+                        stopTimer();
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 用户取消退出，继续计时器
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+    private void stopConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme_Dialog);
+        builder.setTitle("Confirmation")
+                .setMessage("Are you want to stop the timer? Finish your job will record it in your list.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 用户确认退出，停止计时器并关闭应用
+                        stopTimer();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 用户取消退出，继续计时器
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
